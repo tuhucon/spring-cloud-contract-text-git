aca5fb773d1d3299837ad0ff5f6aeb985f5e25c4
nguyen minh tuan <minhtuan93@yahoo.com>
2019-08-13 11:34:11 +0700

    add stub

---
+++ src/main/java/com/example/springcloudcontracttextgit/SpringCloudContractTextGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudcontracttextgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractTextGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudContractTextGitApplication.class, args);
+    }
+
+}
